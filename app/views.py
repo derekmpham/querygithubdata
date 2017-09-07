d92b809bc8523ae4902dd7870ca1bd55f0e74456
Derek Pham <derekmpham@gmail.com>
2017-09-06 20:18:11 -0700

    Create second view

--- app/views.py
+++ app/views.py
@@ -6,3 +6,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
 	return HttpResponse('Hello World!')
+
+def test(request):
+	return HttpResponse('My second view!')
