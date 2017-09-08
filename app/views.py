31bd4ffa60f79c54c46554304a91fee0bc482418
Derek Pham <derekmpham@gmail.com>
2017-09-07 20:59:46 -0700

    Write method to load json strings

--- app/views.py
+++ app/views.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, HttpResponse
+import requests
+import json
 
 # Create your views here.
 def index(request):
@@ -9,3 +11,8 @@ def index(request):
 
 def test(request):
 	return HttpResponse('My second view!')
+
+def profile(request):
+	req = requests.get('https://api.github.com/users/derekmpham')
+	content = req.text
+	return HttpResponse(content)
