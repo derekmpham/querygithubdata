be3059aa7121d98c625c56e093124b71abde50bd
Derek Pham <derekmpham@gmail.com>
2017-09-22 23:49:06 -0700

    Revert to previous static root state that allowed heroku deployment

--- querygithubdata/settings.py
+++ querygithubdata/settings.py
@@ -118,9 +118,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-# PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-# STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-STATIC_ROOT = [os.path.join(BASE_DIR, 'templates')]
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 
-# TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
