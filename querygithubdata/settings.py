604870a47f5573293666e1e1cc39e9a14228581b
Derek Pham <derekmpham@gmail.com>
2017-09-25 10:36:00 -0700

    Include whitenoise t to wsgi file

--- querygithubdata/settings.py
+++ querygithubdata/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = 'n8n4g)&7wdk^!juuf3^5x*^q=8_v74jumn*@q0-&4^*&yaks_&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
