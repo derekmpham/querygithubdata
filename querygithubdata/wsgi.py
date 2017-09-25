567b5e40ab7e3106295c4e870ec768f5a5f81b3e
Derek Pham <derekmpham@gmail.com>
2017-09-24 22:51:11 -0700

    Revert "Add production settings file"
    This reverts commit ff53e39080a29e2d942a38dffbf9647579ab4321.

--- querygithubdata/wsgi.py
+++ querygithubdata/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "querygithubdata.settings_production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "querygithubdata.settings")
 
 application = get_wsgi_application()
