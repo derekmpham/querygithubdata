0baa588a1b39f93a66d79a8c7d6cf4444277baa3
Derek Pham <derekmpham@gmail.com>
2017-09-24 22:51:31 -0700

    Revert "Create empty static folder in general directory to run inner static folder with debug set to false"
    This reverts commit 4316d7ed96b0685ed011d13cc1efd0e0181f18a6.

--- querygithubdata/settings.py
+++ querygithubdata/settings.py
@@ -118,12 +118,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-# ***what we used last-->STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATIC_ROOT = ''
 STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
